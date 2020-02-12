9af5917e7a383bd8652ee6a4f3127ea918a70991
Siwin Lo <siwin.lo@gmail.com>
2020-02-12 11:41:50 -0500

    feat: FetchTitles.py

---
+++ FetchAllProducts.js
@@ -0,0 +1,16 @@
+const Axios = require('axios');
+
+async function fetchAllProducts() {
+	try {
+		const response = await Axios.get(
+			`https://sokoglam.com/products.json?limit=250`,
+		);
+		const data = await response.data.products;
+		const titles = data.map(product => product.title);
+		console.log(titles);
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+fetchAllProducts();
