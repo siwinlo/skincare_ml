dc8a84f349780b1744848fb671355917db7d0933
Siwin Lo <siwin.lo@gmail.com>
2020-02-12 23:05:02 -0500

    new prs

--- exh_ml/exh_generate.py
+++ exh_ml/exh_generate.py
@@ -2,4 +2,6 @@ from textgenrnn import textgenrnn
 
 # Artforum press releases
 t = textgenrnn("textgenrnn_weights.hdf5")
-t.generate_to_file("generated_press.txt", n=10, temperature=0.2)
+t.generate_to_file(
+    "generated_press.txt", n=10, temperature=0.3, prefix="\nThis exhibition"
+)
