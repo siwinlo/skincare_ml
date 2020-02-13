239954b03d0aa0d5bd1d873554637b90c5cf3550
Siwin Lo <siwin.lo@gmail.com>
2020-02-13 12:18:32 -0500

    Updates

---
+++ precache-manifest.c12caf763f3ae2aaf53ced373a52bfa0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06b685f994b53fcb83c0c6851f6be3f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e5cec6def01751e1f2b",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fae16f353c11886dc0ca",
+    "url": "/static/js/2.2abc63e9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.2abc63e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e5cec6def01751e1f2b",
+    "url": "/static/js/main.2582ca02.chunk.js"
+  },
+  {
+    "revision": "d227e76cfa3d0a2c6ccb",
+    "url": "/static/js/runtime-main.71f02b76.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
