aa23a315bb3ec17e721b6c8a3fdf296af4ea3770
Siwin Lo <siwin.lo@gmail.com>
2020-02-12 23:31:52 -0500

    new results

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # skincare_ml
 
 A text generation project.
+
+[generated products](titles/generated_products.txt)
+[generated descriptions](descriptions/generated_descriptions.txt)
+[generated exhibition press release excerpts](exh_ml/all_press_releases.txt)
--- exh_ml/exh_generate.py
+++ exh_ml/exh_generate.py
@@ -3,5 +3,5 @@ from textgenrnn import textgenrnn
 # Artforum press releases
 t = textgenrnn("textgenrnn_weights.hdf5")
 t.generate_to_file(
-    "generated_press.txt", n=10, temperature=0.3, prefix="\nThis exhibition"
+    "generated_press.txt", n=100, temperature=0.3, prefix="\nThis exhibition"
 )
