b01c665adddb5b1cf3f5eb7c2253913ded17d55e
Siwin Lo <siwin.lo@gmail.com>
2020-02-14 17:56:14 -0500

    feat: updated about

--- docs/src/Components/About.js
+++ docs/src/Components/About.js
@@ -3,7 +3,19 @@ import React from 'react';
 function About() {
 	return (
 		<div className="all-container">
-			<p>A text generation project</p>
+			<p>
+				A text generation project
+				<a href="https://siwinlo.github.io"> Siwin Lo</a>
+			</p>
+			<p>
+				This application displays text generated using textgenrnn, a text
+				generation machine learning library, trained on art exhibition press
+				releases drawn from Artforum's Art Guide listings, and skincare product
+				names and descriptions from Soko Glam. However, the samples shown on
+				this UI are drawn from a static bank that is sporadically updated, since
+				hosting the computation of these models is not somthing I have figured
+				out how to do for free at this time.
+			</p>
 		</div>
 	);
 }
