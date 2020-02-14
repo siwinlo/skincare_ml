3ddf658f1e54aeacd3fc453f6de51502147225bb
Siwin Lo <siwin.lo@gmail.com>
2020-02-14 14:28:54 -0500

    added navigation and about

--- docs/src/Components/Main.js
+++ docs/src/Components/Main.js
@@ -7,15 +7,40 @@ import SynthProducts from './SynthProducts';
 class Main extends React.Component {
 	constructor() {
 		super();
-		this.state = {};
+		this.state = {
+			about: false,
+			products: false,
+			exhs: false,
+		};
+	}
+
+	toggleAbout(event) {
+		this.setState({
+			about: !this.state.about,
+		});
+	}
+	toggleProducts(event) {
+		this.setState({
+			products: !this.state.products,
+		});
+	}
+	toggleExhs(event) {
+		this.setState({
+			exhs: !this.state.exhs,
+		});
 	}
 	render() {
 		return (
 			<div className="main">
-				{/* <AllTitles />
-				<AllDescriptions /> */}
-				<SynthProducts />
-				<AllExhs />
+				<div onClick={this.toggleAbout}>
+					{this.state.about ? <About /> : <h1>About</h1>}
+				</div>
+				<div onClick={this.toggleProducts}>
+					{this.state.about ? <SynthProducts /> : <h1>Skincare Products</h1>}
+				</div>
+				<div onClick={this.toggleExhs}>
+					{this.state.about ? <AllExhs /> : <h1>Exhibition Press Releases</h1>}
+				</div>
 			</div>
 		);
 	}
