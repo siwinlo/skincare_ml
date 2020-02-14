3838f9c36577ed261ec8b0c91afee5f0965d3779
Siwin Lo <siwin.lo@gmail.com>
2020-02-14 12:15:12 -0500

    Updates

---
+++ precache-manifest.8b3cee76ee541035c965442709c8f63b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5daef1d6cd74de12724e881bf006ae78",
+    "url": "/skincare_ml/index.html"
+  },
+  {
+    "revision": "daefd4d2f1844ff39aeb",
+    "url": "/skincare_ml/static/css/main.b87d18ae.chunk.css"
+  },
+  {
+    "revision": "f2e3fcd86f6969e16234",
+    "url": "/skincare_ml/static/js/2.9d77b04d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/skincare_ml/static/js/2.9d77b04d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daefd4d2f1844ff39aeb",
+    "url": "/skincare_ml/static/js/main.4a7a9789.chunk.js"
+  },
+  {
+    "revision": "a7d4aaec010fc44aae55",
+    "url": "/skincare_ml/static/js/runtime-main.b9a148f7.js"
+  },
+  {
+    "revision": "7cb401937b6a67eb82ee536d17c12a2a",
+    "url": "/skincare_ml/static/media/generated_descriptions.7cb40193.txt"
+  },
+  {
+    "revision": "28059b40944b93696d5358cf9d8c22f0",
+    "url": "/skincare_ml/static/media/generated_press.28059b40.txt"
+  },
+  {
+    "revision": "504adff49352bc62c7f0c6efc73296cc",
+    "url": "/skincare_ml/static/media/generated_products.504adff4.txt"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/skincare_ml/static/media/logo.5d5d9eef.svg"
+  }
+]);
