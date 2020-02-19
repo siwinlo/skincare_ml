ecf2a3c981fddb8a792a1dda5bce74d79ecaf7d9
Siwin Lo <siwin.lo@gmail.com>
2020-02-19 11:53:56 -0500

    added more generated prs

--- exh_ml/bot/twitterbot_textfile.py
+++ exh_ml/bot/twitterbot_textfile.py
@@ -1,32 +1,34 @@
 import tweepy
 from time import sleep
+from credentials import *
+import random
 
-# from credentials import *
 
-
-# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-# auth.set_access_token(access_token, access_token_secret)
-# api = tweepy.API(auth)
+auth = tweepy.OAuthHandler(key, secret_key)
+auth.set_access_token(token, token_secret)
+api = tweepy.API(auth)
 
 
 def post_tweet():
-    with open("../generated_press.txt") as t:
-        pr = t.readline()
-        i = 1
-        while pr:
-            print("{}".format(pr))
-            pr = t.readline()
-            i += 1
-            # try:
-            #     print(res)
-            #     if res != "":
-            #         api.update_status(res)
-            #         sleep(5000)
-            #     else:
-            #         pass
-            # except tweepy.TweepError as e:
-            #     print(e.reason)
-            #     sleep(2)
+    list_of_prs = []
+    with open("../generated_press.txt", "r") as t:
+        for line in t:
+            list_of_prs.append(line)
+    i = 1
+    while i < 3:
+        res = random.choice(list_of_prs)[:135]
+        i += 1
+        try:
+            print(res)
+            if res != "":
+                api.update_status(res)
+                print("TWEET: ", res)
+                sleep(3600)
+            else:
+                pass
+        except tweepy.TweepError as e:
+            print(e.reason)
+            sleep(2)
 
 
 post_tweet()
--- exh_ml/exh_generate.py
+++ exh_ml/exh_generate.py
@@ -2,9 +2,12 @@ from textgenrnn import textgenrnn
 
 # Artforum press releases
 t = textgenrnn("textgenrnn_weights.hdf5")
-batch = t.generate(n=10, return_as_list=True, temperature=0.2, prefix="The i")
+batch = t.generate(n=10, return_as_list=True, temperature=0.1, prefix="W")
 
-with open("../docs/src/generated_press.txt", "a") as myfile:
+# with open("../docs/src/generated_press.txt", "w") as myfile:
+#     for line in batch:
+#         myfile.write("\n" + line)
+
+with open("./generated_press.txt", "a") as myfile:
     for line in batch:
         myfile.write("\n" + line)
-
