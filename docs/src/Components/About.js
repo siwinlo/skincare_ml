1ba857d53290d2480429ab1d6f3875b7a3d85941
Siwin Lo <siwin.lo@gmail.com>
2020-02-19 12:03:40 -0500

    added link to textgenrnn

--- docs/src/Components/About.js
+++ docs/src/Components/About.js
@@ -14,10 +14,11 @@ function About() {
 				generation machine learning library, trained on art exhibition press
 				releases drawn from Artforum's Art Guide listings, and skincare product
 				names and descriptions from Soko Glam. However, the samples shown on
-				this UI are drawn from a static bank that is sporadically updated, since
-				hosting the computation of these models is not somthing I have figured
-				out how to do for free at this time.
+				this UI are drawn from a static bank that is sporadically updated.
 			</p>
+			<a href="https://github.com/minimaxir/textgenrnn">
+				Learn more about textgenrnn
+			</a>
 		</div>
 	);
 }
